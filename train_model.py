b25f3014b1ecde1c62f21e270226214474d77116
Hamza Almhameed <143035357+Hamza-Almhameed@users.noreply.github.com>
2026-02-21 11:39:58 +0300

    Add confirmation message after model saving
    Added a confirmation message after saving the model.

--- train_model.py
+++ train_model.py
@@ -3,24 +3,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import joblib
 
-# تحميل الداتا
 digits = load_digits()
 
 X = digits.data
 y = digits.target
 
-# تقسيم البيانات
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# موديل بسيط وقوي
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train, y_train)
 
 print("Accuracy:", model.score(X_test, y_test))
 
-# حفظ الموديل
 joblib.dump(model, "digit_model.pkl")
+
 
 print("Model saved!")
